c8dd09a4a345028685ea97379e475a19820bf0a9
Mehedi Hasan <MehediHasan10>
2020-10-24 16:39:39 +0600

    Initial Setup

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
+//Mongodb config and setup
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+};
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).catch((err) => {
+    console.log(err); //Handles initial connection error
+});
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log('> Error occurred from database !'));
+db.once('open', () => console.log('> Successfully connected to database...'));
+
+//View Engine Setup
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
+//Routing handler
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+//Setup the public folder. WE are telling the express, where the public files are going to be stored.
+app.use(express.static("./public"));
+
+//Server Listener
+const port = process.env.PORT || 4000;
+app.listen(port, ()=> console.log(`Server is running at port ${port}...`));
+
+
+
+
